cbd0a730599038641972896a0ccd20a24a594471
gaojuan <2769442614@qq.com>
2017-06-10 13:46:37 +0800

    fsafdas

--- TestGCD/ViewController.m
+++ TestGCD/ViewController.m
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"第一个");
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"第二个");
+    });
+    NSLog(@"第三个");
 }
 
 
